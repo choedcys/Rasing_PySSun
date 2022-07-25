6d4fafef0c5a59caa8f644ba745ca03b44f65a18
choedcys <choedcys@naver.com>
2022-07-25 10:08:39 +0900

    시각 내 답안

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-destin=[]
-i = 0
-pathx = 1
-pathy = 1
-width = int(input("가로 좌표: "))
-height = int(input("세로 좌표: "))
-
-while 1:
-    temp = input("오른쪽:R 왼쪽:L 위:U 아래:D 종료:0")
-    if temp == '0':
-        break
-    destin.append(temp)
-    i = i+1
-
-for i in range(0,len(destin)):
-    if destin[i] == 'R':
-        if pathx%width != height:
-            pathx=pathx+1
-    elif destin[i] == 'L':
-        if pathx % width != 0:
-            pathx = pathx - 1
-    elif destin[i] == 'U':
-        if pathy > 1:
-            pathy=pathy-1
-    else:
-        if(pathy <= height-2):
-            pathy=pathy+1
-print("시작: (1, 1)")
-print("위치: (",pathx,", ",pathy,")")
+x = int(input("시간입력: "))
+sec = 3600*x + 59*60 +59
+count = 0
+for i in range(0, sec+1):
+    temp = i
+    h = temp // 3600
+    temp -= h*3600
+    m = temp //60
+    temp -= m*60
+    if h % 10 == 3 or h//10 == 3 or m % 10 == 3 or m//10 == 3 or temp % 10 == 3 or temp//10 == 3:
+        print(h,"시 ",m,"분 ", temp,"초")
+        count += 1
+print(count)
